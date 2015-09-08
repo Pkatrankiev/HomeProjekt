44f0b91ac02f7f0f1c24c0918f7fc6ae9389ab32
pkatrankiev <pkatrankiev@abv.bg>
2015-09-08 20:57:46 +0300

    добавяне на текст със заданието на задачата

--- Homework-OOP4/src/DoublyLinkedList/package-info.java
+++ Homework-OOP4/src/DoublyLinkedList/package-info.java
@@ -1,8 +1,10 @@
 /**
- * 
- */
-/**
- * @author ibm
- *
+ Напишете реализация на списък в Java, като използвате
+двойно свързан списък. Нека да има следните методи:
+- int size()
+- boolean isEmpty()
+- boolean add(Object element)
+- boolean remove(Object element)
+- boolean contains(Object element)
  */
 package DoublyLinkedList;
